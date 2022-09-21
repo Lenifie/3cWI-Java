97bb5e3b0b3768c2a1246e58e81b2ae6bf11e86b
Lenifie <leoniesophie.stecher@gmx.at>
2022-09-21 10:46:36 +0200

    versuch1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        //int - ganze Zahl
+        //float - kommazahl
+        //double - kommazahl (groß)
+        //boolean - true/false
+        //char - zeichen
+        //string - zeichenkette
+
+        int number = 16;
+        String name = "Leonie";
+
+        System.out.println(name);
+        System.out.println(number);
+
+
+
+
+    }
+
+    //strg blank
+}
