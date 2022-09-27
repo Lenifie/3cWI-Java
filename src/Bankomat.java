a38566fb3883ac33ed3190e375f1a4eaeab35fea
Lenifie <leoniesophie.stecher@gmx.at>
2022-09-27 16:18:51 +0200

    Auswahlmenü

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -1,5 +1,17 @@
 public class Bankomat {
     public static void main(String[] args) {
 
+        int einzahlen = 0;
+        int abheben = 0;
+        int kontostand = 0;
+        int beenden = 0;
+
+        System.out.println("Auswahlmenü:");
+        System.out.println("1. Einzahlen (e)");
+        System.out.println("2. Abheben (a)");
+        System.out.println("3. Kontostand (k)");
+        System.out.println("4. Beenden (b)");
+
+
     }
 }
