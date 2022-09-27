5877938f16470a4059fbe654e6892fb8968ce3fd
Lenifie <leoniesophie.stecher@gmx.at>
2022-09-27 16:34:11 +0200

    switch

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bankomat {
     public static void main(String[] args) {
 
@@ -5,13 +7,38 @@ public class Bankomat {
         int abheben = 0;
         int kontostand = 0;
         int beenden = 0;
-
+        Scanner eingabe = new Scanner(System.in);
+        int eingabe = eingabe.nextInte();
+        Scanner auswahl = new Scanner(System.in);
+        boolean auswahl = auswahl.nextBoolean();
         System.out.println("Auswahlmenü:");
         System.out.println("1. Einzahlen (e)");
         System.out.println("2. Abheben (a)");
         System.out.println("3. Kontostand (k)");
         System.out.println("4. Beenden (b)");
+        auswahl.nextBoolean();
 
+        switch (auswahl)
+        {
+            case e:
+                System.out.println("Geldmenge: ");
+                eingabe.nextInt(einzahlen);
+                kontostand = kontostand + einzahlen;
+                System.out.println(kontostand);
 
+                break;
+            case a:
+                System.out.println("Geldmenge: ");
+                eingabe.nextInt(abheben);
+                kontostand = kontostand - abheben;
+                break;
+            case k:
+                System.out.println("Der kontostand ist: ");
+                System.out.println(kontostand);
+                break;
+            case b:
+                System.out.println("Wird beendet ...");
+                break;
+        }
     }
 }
