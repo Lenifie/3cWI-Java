dacfc785da52f59879e1bca3d6cbf1e6687f6172
Lenifie <leoniesophie.stecher@gmx.at>
2022-09-28 10:57:34 +0200

    added Beenden of Game

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -3,42 +3,44 @@ import java.util.Scanner;
 public class Bankomat {
     public static void main(String[] args) {
 
-        int einzahlen = 0;
-        int abheben = 0;
-        int kontostand = 0;
-        int beenden = 0;
-        Scanner eingabe = new Scanner(System.in);
-        int eingabe = eingabe.nextInte();
-        Scanner auswahl = new Scanner(System.in);
-        boolean auswahl = auswahl.nextBoolean();
-        System.out.println("Auswahlmenü:");
-        System.out.println("1. Einzahlen (e)");
-        System.out.println("2. Abheben (a)");
-        System.out.println("3. Kontostand (k)");
-        System.out.println("4. Beenden (b)");
-        auswahl.nextBoolean();
 
-        switch (auswahl)
-        {
-            case e:
-                System.out.println("Geldmenge: ");
-                eingabe.nextInt(einzahlen);
-                kontostand = kontostand + einzahlen;
-                System.out.println(kontostand);
+        int balance = 0;
+        boolean isProgrammRunning = true;
 
-                break;
-            case a:
-                System.out.println("Geldmenge: ");
-                eingabe.nextInt(abheben);
-                kontostand = kontostand - abheben;
-                break;
-            case k:
-                System.out.println("Der kontostand ist: ");
-                System.out.println(kontostand);
-                break;
-            case b:
-                System.out.println("Wird beendet ...");
-                break;
-        }
+
+       while(isProgrammRunning){
+           System.out.println("Auswahlmenü:");
+           System.out.println("1. Einzahlen (e)");
+           System.out.println("2. Abheben (a)");
+           System.out.println("3. Kontostand (k)");
+           System.out.println("4. Beenden (b)");
+
+           Scanner inputFromScanner = new Scanner(System.in);
+
+           char selectionOfUser = inputFromScanner.next().charAt(0);
+           System.out.println(selectionOfUser);
+
+           switch (selectionOfUser)
+           {
+               case 'e':
+                   System.out.println("Geldmenge: ");
+                   int value = inputFromScanner.nextInt();
+                   balance += value;
+                   break;
+               case 'a':
+                   System.out.println("Geldmenge: ");
+//                eingabe.nextInt(abheben);
+//                kontostand = kontostand - abheben;
+                   break;
+               case 'k':
+                   System.out.println("Der kontostand ist: ");
+                   System.out.println(balance);
+                   break;
+               case 'b':
+                   System.out.println("Wird beendet ...");
+                   isProgrammRunning = false;
+                   break;
+           }
+       }
     }
 }
