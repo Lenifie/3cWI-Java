1c2c36fcbe08d1a89dbfe0476612463fed3f1161
Lenifie <leoniesophie.stecher@gmx.at>
2022-10-17 12:53:06 +0200

    added OO 03

--- src/at/stl/objects/Car.java
+++ src/at/stl/objects/Car.java
@@ -2,15 +2,36 @@ package at.stl.objects;
 
 public class Car {
     // Instanz / Gedächtnisvariablen
-    public int fuelConsumption;
-    public String brand;
-    public String serialNumber;
-    public String color;
-    public int fuelAmount;
+    public int fuelConsumption; //Verbrauch pro km
+    public String brand; //Marke
+    public String serialNumber; //Seriennummer
+    public String color; //Autofarbe
+    public int fuelAmount; //Tankstand
+    public int amountOfRepetitions; //Anzahl hupen
 
     //Methode
     public void drive(){
         this.fuelAmount = this.fuelAmount -fuelConsumption;
         System.out.println("I am driving");
+    }
+
+    public void brake(){
+        System.out.println("I brake");
+    }
+    public void turboBoost(){
+        if (this.fuelAmount > (this.fuelAmount * 0.1)){
+            System.out.println("SuperBoostMode");
+        }else{
+            System.out.println("Not enough fuel to go to Superboost");
+        }
+    }
+    public void honk(){
+        for (int i = 0; i< amountOfRepetitions; i++) {
+            System.out.println("Tuuut");
+        }
+    }
+    public void getRemainingRange(){
+        int remainingRange = this.fuelAmount/ this.fuelConsumption;
+        System.out.println(remainingRange);
     }
 }
